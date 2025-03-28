302442267403e1d800ece11f9a4bd5fd61a96150
emresurmeli <s.emre.s.8@gmail.com>
2025-03-28 09:59:55 -0400

    Modernize the men-stack-auth-template, add chalk, .env.sample, and a .gitignore

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-const dotenv = require('dotenv');
+import dotenv from "dotenv";
 dotenv.config();
-const express = require('express');
+import methodOverride from "method-override";
+import morgan from "morgan";
+import session from "express-session";
+import express from "express";
+import authController from "./controllers/auth.js";
+import "./db/connection.js";
+
 const app = express();
-const mongoose = require('mongoose');
-const methodOverride = require('method-override');
-const morgan = require('morgan');
-const session = require('express-session');
-
-const authController = require('./controllers/auth.js');
-
-const port = process.env.PORT ? process.env.PORT : '3000';
-
-mongoose.connect(process.env.MONGODB_URI);
-
-mongoose.connection.on('connected', () => {
-  console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
-});
+const port = process.env.PORT ? process.env.PORT : "3000";
 
 app.use(express.urlencoded({ extended: false }));
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 // app.use(morgan('dev'));
 app.use(
   session({
@@ -28,21 +21,21 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
+app.get("/", (req, res) => {
+  res.render("index.ejs", {
     user: req.session.user,
   });
 });
 
-app.get('/vip-lounge', (req, res) => {
+app.get("/vip-lounge", (req, res) => {
   if (req.session.user) {
     res.send(`Welcome to the party ${req.session.user.username}.`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    res.send("Sorry, no guests allowed.");
   }
 });
 
-app.use('/auth', authController);
+app.use("/auth", authController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
